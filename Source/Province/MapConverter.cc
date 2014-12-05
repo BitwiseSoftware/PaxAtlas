3d8a1759f815c14f0cdada1c0fd70845b6201027
Justin Harrison <justin@pyrohail.com>
2014-12-05 13:27:40 -0500

    Implement convex province shapes

--- Source/Province/MapConverter.cc
+++ Source/Province/MapConverter.cc
@@ -1,7 +1,6 @@
 #include "MapConverter.hh"
 
 #include "Province.hh"
-#include <iostream>
 
 PA::Province::MapConverter::MapConverter()
 {
@@ -23,14 +22,20 @@ void PA::Province::MapConverter::init_tiles()
 			}
 		}
 	}
+
+	for (auto& pair : tiles)
+	{
+		auto& pixels = pair.second;
+		pixels = convex_hull(pixels);
+	}
 }
 
 void PA::Province::MapConverter::create_shape()
 {
 	for (const auto& pair : tiles)
 	{
-		const auto color = pair.first;
-		const auto pixels = pair.second;
+		const auto& color = pair.first;
+		const auto& pixels = pair.second;
 
 		sf::ConvexShape shape;
 		shape.setPointCount(pixels.size());
@@ -44,4 +49,34 @@ void PA::Province::MapConverter::create_shape()
 		auto province = new PA::Province::Province;
 		province->set_shape(shape);
 	}
+}
+
+// http://en.wikibooks.org/wiki/Algorithm_Implementation/Geometry/Convex_hull/Monotone_chain#C.2B.2B
+int PA::Province::MapConverter::cross(const sf::Vector2f &O, const sf::Vector2f &A, const sf::Vector2f &B)
+{
+	return (A.x - O.x) * (B.y - O.y) - (A.y - O.y) * (B.x - O.x);
+}
+
+//http://en.wikibooks.org/wiki/Algorithm_Implementation/Geometry/Convex_hull/Monotone_chain#C.2B.2B
+std::vector<sf::Vector2f> PA::Province::MapConverter::convex_hull(std::vector<sf::Vector2f> pixels)
+{
+	int n = pixels.size(), k = 0;
+	std::vector<sf::Vector2f> h(2 * n);
+
+	// Build lower hull
+	for (int i = 0; i < n; ++i)
+	{
+		while (k >= 2 && cross(h[k - 2], h[k - 1], pixels[i]) <= 0) k--;
+		h[k++] = pixels[i];
+	}
+
+	// Build upper hull
+	for (int i = n - 2, t = k + 1; i >= 0; i--)
+	{
+		while (k >= t && cross(h[k - 2], h[k - 1], pixels[i]) <= 0) k--;
+		h[k++] = pixels[i];
+	}
+
+	h.resize(k);
+	return h;
 }
--- Source/Province/MapConverter.hh
+++ Source/Province/MapConverter.hh
@@ -32,6 +32,10 @@ namespace PA {
 
 			void init_tiles();
 			void create_shape();
+
+			// http://en.wikibooks.org/wiki/Algorithm_Implementation/Geometry/Convex_hull/Monotone_chain#C.2B.2B
+			int cross(const sf::Vector2f &O, const sf::Vector2f &A, const sf::Vector2f &B);
+			std::vector<sf::Vector2f> convex_hull(std::vector<sf::Vector2f> pixels);
 		};
 	}
 }
